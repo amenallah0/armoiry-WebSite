93f41bb9e7dddd42a98bb10e6f1852014fcc63db
Amen Allah Jouila <amenallah1991@gmail.com>
2024-04-15 09:58:27 +0100

    Fixing Carousel

--- src/Components/Cards/Card.js
+++ src/Components/Cards/Card.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Carousel } from 'antd';
-import carimg from"../images/Stade1.png"
+
 const contentStyle = {
   Margin: 0,
   height: '160px',
@@ -13,11 +13,9 @@ const contentStyle = {
 };
 
 const App = () => (
-  <div  >
-
-  <Carousel autoplay dotPosition='left' fade='#364d79' style={{minHeight:"100px"}}>
-    <div  >
-      <img src={carimg} alt='' />
+  <Carousel autoplay dotPosition='left' fade='#364d79' >
+    <div>
+      <h3 style={contentStyle}>1</h3>
     </div>
     <div>
       <h3 style={contentStyle}>2</h3>
@@ -29,7 +27,5 @@ const App = () => (
       <h3 style={contentStyle}>4</h3>
     </div>
   </Carousel>
-
-  </div>
 );
 export default App;
